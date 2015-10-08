569759dd9372b4524815a610095d0a72c6508a7e
vladharani <vlad.harani@live.com>
2015-10-08 20:47:41 +0300

    Exercises
    Sper sa nu fie total fail

---
+++ AthleteExercises.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AthleteExercises
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int N = 36;          
+            int NumberOfRepetitions = N*N;
+            Assert.AreEqual(1296, NumberOfRepetitions);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int N = 36;
+            int NumberOfRepetitions = N * N;
+            Assert.AreEqual(14000, NumberOfRepetitions);
+        }
+    }
+}
---
+++ FlooringCalculation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject4
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int Length = 10;
+            int Width = 10;
+            int FlooringLength = 4;
+            int FlooringWidth = 4;
+            double FlooringWithMargin = FlooringLength * FlooringWidth * 25 / 100;
+            double Calculation = (Length * Width / FlooringWithMargin);
+            Assert.AreEqual(25, Calculation);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int Length = 10;
+            int Width = 10;
+            int FlooringLength = 4;
+            int FlooringWidth = 4;
+            double FlooringWithMargin = FlooringLength * FlooringWidth * 25 / 100;
+            double Calculation = (Length * Width / FlooringWithMargin);
+            Assert.AreEqual(40, Calculation);
+        }
+    }
+}
