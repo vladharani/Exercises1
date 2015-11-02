5ff6535897847758bb8d510e0b30e2187ebff9fe
Vlad Harani <vlad.harani@live.com>
2015-11-02 23:47:17 +0200

    Calculate Taxi Fare Exercise

---
+++ FirstTry.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TaxiFareCalcProblem
+{
+    [TestClass]
+    public class FirstTry
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            TaxiFareCalc obj = new TaxiFareCalc();
+            int taxiFare = obj.Calculate(9, 5);
+            Assert.AreEqual(25, taxiFare);
+        }
+        [TestMethod]
+        public void Test2()
+        {
+            TaxiFareCalc obj = new TaxiFareCalc();
+            int taxiFare = obj.Calculate(23, 5);
+            Assert.AreEqual(35, taxiFare);
+        }
+        [TestMethod]
+        public void Test3()
+        {
+            TaxiFareCalc obj = new TaxiFareCalc();
+            int taxiFare = obj.Calculate(23, 40);
+            Assert.AreEqual(400, taxiFare);
+        }
+        [TestMethod]
+        public void Test4()
+        {
+            TaxiFareCalc obj = new TaxiFareCalc();
+            int taxiFare = obj.Calculate(10, 30);
+            Assert.AreEqual(240, taxiFare);
+        }
+        public class TaxiFareCalc
+        {
+            public int Calculate(int hour, int distance)
+            {
+                int price = 0;
+
+                if (hour>8 && hour<21 && distance <=20)
+                {
+                    price = 5;
+                }
+                else if (hour < 8 || hour > 21 && distance <= 20)
+                {
+                    price = 7;
+
+                }
+                else if (hour > 8 && hour < 21 && distance > 20 && distance < 60)
+                {
+                    price = 4;
+                    distance = distance * 2;
+                }
+                else if (hour < 8 || hour > 21 && distance > 20 && distance < 60)
+                {
+                    price = 5;
+                    distance = distance * 2;
+                }
+                else if (hour > 8 && hour < 21 && distance > 60)
+                {
+                    price = 3;
+                    distance = distance * 2;
+                }
+                else if (hour < 8 || hour > 21 && distance > 60)
+                {
+                    price = 4;
+                    distance = distance * 2;
+                }
+                return price * distance;
+            }
+        }
+    }
+}
