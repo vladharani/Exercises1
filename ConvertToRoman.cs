3caed022d1d372f83d535bb91437b19c789899df
Vlad Harani <vlad.harani@live.com>
2015-11-24 21:32:05 +0200

    Cele 2 restante

---
+++ ConvertToRoman.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConvertToRomanNumerals
+{
+    [TestClass]
+    public class ConvertToRoman
+    {
+        [TestMethod]
+        public void TestConversionToRomanNumeralsFromOneToNine()
+        {
+            Assert.AreEqual("IX", NumberToRomanNumerals(9));
+        }
+        [TestMethod]
+        public void TestConversionToRomanNumeralsFromTenToNinetyNine()
+        {
+            Assert.AreEqual("XCIV", NumberToRomanNumerals(94));
+        }
+        [TestMethod]
+        public void TestConversionToRomanNumeralsFromHundredToThousand()
+        {
+            Assert.AreEqual("CMXCIV", NumberToRomanNumerals(994));
+        }
+        [TestMethod]
+        public void TestConversionToRomanNumeralsThousand()
+        {
+            Assert.AreEqual("M", NumberToRomanNumerals(1000));
+        }
+        public string NumberToRomanNumerals(int number)
+        {
+            string[] digits = new string[] { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+            string[] tens = new string[] { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+            string[] hundreds = new string[] { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM", "M" };
+            string result = "";
+
+            if (number > 100)
+            {
+                int wholeHundreds = number / 100;
+                result = hundreds[wholeHundreds];
+                number = number - wholeHundreds * 100;
+            }
+            if (number > 10)
+            {
+                int wholeTens = number / 10;
+                result += tens[wholeTens];
+                number = number - wholeTens * 10;
+            }
+            if (number < 10)
+            {
+                result += digits[number];
+            }
+            return result;
+
+        }
+    }
+}
+
---
+++ CoordinateAreaCalculation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CoordinateAreaCalculationProblem
+{
+    [TestClass]
+    public class CoordinateAreaCalculation
+    {
+        [TestMethod]
+        public void TestCoordinateAreaCalculation()
+        {
+            Assert.AreEqual(18, AreaCalculation(2,2,4,10,8,8),0.01);
+        }
+        public static double AreaCalculation(double ax, double ay, double bx, double by, double cx, double cy)
+        {
+            double area = -(ax * by - ax * cy + bx * cy - bx * ay + cx * ay  - cx * by) / 2;
+            return area;
+        }
+    }
+}
