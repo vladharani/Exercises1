270a69477a84f5071301ca9429cb7cc2aa1b9efa
Vlad Harani <vlad.harani@live.com>
2015-12-01 19:52:57 +0200

    Nu pot sa imi dau seama de ce nu ruleaza testul, la debug imi apar x si y as expected.

---
+++ RoutingPointProblem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RoutingPointProblem
+{
+    [TestClass]
+    public class RoutingCoordinates
+    {
+        [TestMethod]
+        public void TestRoutingCoordinates()
+        {
+            Assert.AreEqual(pointCoordinates(2, 5), pointCoordinates(2, 3));
+        }
+        static object pointCoordinates(int x, int y)
+        {
+
+            string[] commandString = new string[] { "up", "left", "down", "down", "up", "left", "right", "right", "up", "up" };
+
+            for (int index = 0; index < commandString.Length; index++)
+            {
+                if (commandString[index] == "up") { y += 1; }
+                else if (commandString[index] == "down") { y -= 1; }
+                else if (commandString[index] == "right") { x += 1; }
+                else if (commandString[index] == "left") { x -= 1; }
+            }
+            return pointCoordinates(x, y);
+        }
+
+    }
+}
