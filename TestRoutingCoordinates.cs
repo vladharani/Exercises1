1b2e6b705eb3900a9b5f5eb329772b3c51bc0969
Vlad Harani <vlad.harani@live.com>
2015-12-01 18:33:53 +0200

    Routing Point using string commands attempt 1

---
+++ TestRoutingCoordinates.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RoutingPointProblem
+{
+    [TestClass]
+    public class RoutingCoordinates
+    {
+        [TestMethod]
+        public void TestRoutingCoordinates()
+        {
+            Assert.AreEqual(startingPoint(2, 3), startingPoint(2,3));
+        }
+        string[] commandString = new string[] { "up", "left" };
+        static object startingPoint(int x=2, int y=3)
+        {
+            string[] move = new string[] { "up", "left" };
+            int index = 0;
+            while (index < move.Length)
+                {
+                    if (move[index] == "up") { y += 1; }
+                    else if (move[index] == "down") { y -= 1; }
+                    else if (move[index] == "right") { x += 1; }
+                    else x -= 1;
+                
+                }
+            return startingPoint(x, y);
+                    
+        }
+    }
+}
