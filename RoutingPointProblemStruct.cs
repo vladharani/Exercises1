0890932ce017b698096c3929b3cf6d7fe7aa0c09
Vlad Harani <vlad.harani@live.com>
2015-12-06 22:25:15 +0200

    inceput problema routing coordinates using struct

---
+++ RoutingPointProblemStruct.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace RoutingPointProblem
+{
+    [TestClass]
+    public class RoutingCoordinates
+    {
+        [TestMethod]
+        public void TestRoutingCoordinates()
+        {
+            string[] newCommands = new string[] { "up", "right" };
+            NewFindLastPoint(startingPoint, newCommands);
+            Assert.AreEqual(startingPoint(1, 1), startingPoint(0, 0));
+            
+        }
+
+        public struct point
+        {
+            public int x;
+            public int y;
+            public point (int startingX, int startingY)
+            {
+                x = startingX;
+                y = startingY;
+            }
+        }
+        public point startingPoint = new point(0,0);
+        
+        public object NewFindLastPoint(point startingPoint,string[] newCommands)
+        {
+            for (int index = 0; index < newCommands.Length; index++)
+            {
+                if (newCommands[index] == "up") { y = startingY + 1; startingY = y; }
+                else if (newCommands[index] == "down") { y = startingY - 1; startingY = y; }
+                else if (newCommands[index] == "right") { x = startingX + 1; startingX = x; }
+                else if (newCommands[index] == "left") { x = startingX + 1; startingX = x; }
+            }
+        }
+    }
+}
